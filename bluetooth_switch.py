57db246555bb047a39ebb34cb9946e0fa385ba2d
Deepu Nagarajan <nagarajan.deepu@gmail.com>
2021-05-22 20:46:31 +0530

    Removed scripts that have not been fully completed

--- bluetooth_switch.py
+++
@@ -1,3 +0,0 @@
-import subprocess, sys, os
-p = subprocess.Popen(["powershell.exe", r"C:\Users\nagar\Downloads\bluetooth.ps1"], stdout=sys.stdout)
-p.communicate()
--- testgui.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk 
-
-root = tk.Tk()
-label = tk.Label(root, text="Hello World!")
-label.pack(padx=20, pady=20)
-
-root.mainloop()
