8a92971385bea2717845f1cd201d2d979dd9cf8c
Deepu Nagarajan <nagarajan.deepu@gmail.com>
2021-05-22 20:39:07 +0530

    Script to crawl Amazon Website and list the products

--- crawler.py
+++ crawler.py
@@ -2,24 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-url = "https://www.flipkart.com/laptops/pr?sid=6bo,b5g"
-
+url = 'https://www.amazon.in/gp/browse.html?node=1375424031&ref_=nav_em_sbc_mobcomp_laptops_0_2_8_15'
 response=requests.get(url,headers=headers)
-soup = BeautifulSoup(response.content,'lxml')
-print("soup",soup)
+soup=BeautifulSoup(response.content,'lxml')
 i = 0
-for item in soup.select('_1AtVbE'):
-    try:
-        print('----------------------------------------')
-        #print(item)
-        i=i+1
-        print(i)
-        print(item.select('_4rR01T')[0].get_text().strip()) #Name of Product
-        print(item.select('_30jeq3 _1_WHN1')[0].get_text().strip()) #Price
+for item in soup.select('.zg-item-immersion'):
+  try:
+    print('----------------------------------------')
+    #print(item)
+    i=i+1
+    print(i)
+    print(item.select('.p13n-sc-truncate')[0].get_text().strip())
+    print(item.select('.p13n-sc-price')[0].get_text().strip())
     #print(item.select('.a-icon-row i')[0].get_text().strip())
     #print(item.select('.a-icon-row a')[1].get_text().strip())
     #print(item.select('.a-icon-row a')[1]['href'])
-        print(item.select('_396cs4 _3exPp9')[0]['src']) #image
-    except Exception as e:
+    print(item.select('img')[0]['src'])
+  except Exception as e:
     #raise e
-        print('')
+    print('')
